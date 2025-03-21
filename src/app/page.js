64b66af4f92ff586fc62aa95fe928634fbf196da
anoverga <gabrielanover17@gmail.com>
2025-03-21 05:45:18 -0400

    Reworked layout of about page

--- src/app/page.js
+++ src/app/page.js
@@ -1,21 +1,23 @@
-import Intro from "@/components/intro";
 import styles from "./page.module.css";
+import Image from 'react-bootstrap/Image';
 
 export default function Home() {
   return (
     <div className={styles.page}>
-      <Intro/>
-      <div className={styles.about_section}>
+        <div className={styles.profile_message}>
+            Hello World
+        </div>
+        <Image className={styles.profile_img} src="gabriel-1.jpg"/>
         <div className={styles.bio}>
           Hello, I am Gabriel Libozada Anover. I am a Canadian software engineer currently based in Tokyo. 
           I have recently graduated with distinction from the University of Toronto, having done a specialist in Computer Science. 
           I have a passion for and specialize in frontend software engineering and low level systems programming. I am currently looking
-          for roles that would allow me to work in either of these disciplines. <br/><br/>
+          for roles that would allow me to work in either of these disciplines.
+          <br/>
           In my spare time, I also enjoy going outdoors to do Astronomy and partaking in the traditional Japanese
-          arts of Iaido and Kenjutsu (swordsmanship). 
+          arts of Iaido and Kenjutsu (swordsmanship).
         </div>
-        <div className={styles.about_title} href="#about">About Me</div>
-      </div>
+        <div id="about" className={styles.about_title}>About Me</div>
     </div>
   );
 }
